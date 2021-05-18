87279bd4483311c197cd10c24e32497730aa062c
ScottAgirs <scott@ijs.to>
2021-05-17 21:14:44 -0400

    chore: build
    Signed-off-by: ScottAgirs <scott@ijs.to>

--- index.js
+++ index.js
@@ -9,47 +9,9 @@ var _react = _interopRequireDefault(require("react"));
 
 var _styledComponents = _interopRequireDefault(require("styled-components"));
 
+var _templateObject, _templateObject2, _templateObject3, _templateObject4;
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-function _templateObject4() {
-  var data = _taggedTemplateLiteral(["\n  appearance: none;\n  position: absolute;\n  right: 10px;\n  top: 10px;\n"]);
-
-  _templateObject4 = function _templateObject4() {
-    return data;
-  };
-
-  return data;
-}
-
-function _templateObject3() {
-  var data = _taggedTemplateLiteral(["\n  background: white;\n  height: 100vh;\n  ", "\n  overflow-y: auto;\n  position: fixed;\n  transition: all ", ";\n  width: ", ";\n  z-index: 999;\n  ", ";\n"]);
-
-  _templateObject3 = function _templateObject3() {
-    return data;
-  };
-
-  return data;
-}
-
-function _templateObject2() {
-  var data = _taggedTemplateLiteral(["\n  background: rgba(\n    ", ",\n    ", "\n  );\n  display: ", ";\n  height: 100vh;\n  left: 0;\n  opacity: ", ";\n  position: fixed;\n  top: ", ";\n  transition: all 0.5s ease-in-out;\n  width: 100vw;\n  z-index: 998;\n"]);
-
-  _templateObject2 = function _templateObject2() {
-    return data;
-  };
-
-  return data;
-}
-
-function _templateObject() {
-  var data = _taggedTemplateLiteral(["\n  display: block;\n  top: ", ";\n  position: fixed;\n  z-index: 997;\n"]);
-
-  _templateObject = function _templateObject() {
-    return data;
-  };
-
-  return data;
-}
 
 function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
@@ -82,52 +44,65 @@ function hexToRgb(hex) {
   return null;
 }
 
-var StyledWrapper = _styledComponents["default"].div(_templateObject(), function (_ref) {
+var StyledWrapper = _styledComponents["default"].div(_templateObject || (_templateObject = _taggedTemplateLiteral(["\n  display: block;\n  top: ", ";\n  position: fixed;\n  z-index: ", ";\n"])), function (_ref) {
   var fromTop = _ref.fromTop;
   return fromTop;
+}, function (_ref2) {
+  var zIncrement = _ref2.zIncrement;
+  return zIncrement + 7;
 });
 
-var StyledOverlay = _styledComponents["default"].div(_templateObject2(), function (_ref2) {
-  var overlayColor = _ref2.overlayColor;
+var StyledOverlay = _styledComponents["default"].div(_templateObject2 || (_templateObject2 = _taggedTemplateLiteral(["\n  background: rgba(\n    ", ",\n    ", "\n  );\n  display: ", ";\n  height: 100vh;\n  left: 0;\n  opacity: ", ";\n  position: fixed;\n  top: ", ";\n  transition: all 0.5s ease-in-out;\n  width: 100vw;\n  z-index: ", ";\n"])), function (_ref3) {
+  var overlayColor = _ref3.overlayColor;
   return overlayColor ? hexToRgb(overlayColor) : '0,0,0';
-}, function (_ref3) {
-  var overlayOpacity = _ref3.overlayOpacity;
-  return overlayOpacity || '0.5';
 }, function (_ref4) {
-  var isOpen = _ref4.isOpen;
-  return isOpen ? 'block' : 'none';
+  var overlayOpacity = _ref4.overlayOpacity;
+  return overlayOpacity || '0.5';
 }, function (_ref5) {
   var isOpen = _ref5.isOpen;
-  return isOpen ? '1' : '0';
+  return isOpen ? 'block' : 'none';
 }, function (_ref6) {
-  var fromTop = _ref6.fromTop;
+  var isOpen = _ref6.isOpen;
+  return isOpen ? '1' : '0';
+}, function (_ref7) {
+  var fromTop = _ref7.fromTop;
   return fromTop;
+}, function (_ref8) {
+  var zIncrement = _ref8.zIncrement;
+  return zIncrement + 8;
 });
 
-var StyledSlideOut = _styledComponents["default"].div(_templateObject3(), function (_ref7) {
-  var padding = _ref7.padding;
-  return padding && "padding: ".concat(padding, ";");
-}, function (_ref8) {
-  var speed = _ref8.speed;
-  return speed || '0.3s';
-}, function (_ref9) {
-  var width = _ref9.width;
-  return width;
+var StyledSlideOut = _styledComponents["default"].div(_templateObject3 || (_templateObject3 = _taggedTemplateLiteral(["\n  /* background: ", "; */\n  background: goldenrod;\n  height: 100vh;\n  ", "\n  overflow-y: auto;\n  position: fixed;\n  transition: all ", ";\n  width: ", ";\n  z-index: ", ";\n  ", ";\n"])), function (_ref9) {
+  var bg = _ref9.bg;
+  return bg;
 }, function (_ref10) {
-  var fromTop = _ref10.fromTop,
-      slideFrom = _ref10.slideFrom,
-      isOpen = _ref10.isOpen;
+  var padding = _ref10.padding;
+  return padding && "padding: ".concat(padding, ";");
+}, function (_ref11) {
+  var speed = _ref11.speed;
+  return speed || '0.3s';
+}, function (_ref12) {
+  var width = _ref12.width;
+  return width;
+}, function (_ref13) {
+  var zIncrement = _ref13.zIncrement;
+  return zIncrement + 9;
+}, function (_ref14) {
+  var fromTop = _ref14.fromTop,
+      slideFrom = _ref14.slideFrom,
+      isOpen = _ref14.isOpen;
   return slideFrom === 'left' ? "transform: ".concat(isOpen ? 'translateX(0%)' : 'translateX(-100%)', ";\n          left: 0") : slideFrom === 'right' ? "transform: ".concat(isOpen ? 'translateX(0%)' : 'translateX(100%)', ";\n          right: 0") : slideFrom === 'top' ? "transform: ".concat(isOpen ? 'translateY(0%)' : 'translateY(-100%)', ";\n          top: ").concat(fromTop, "; left: 0; bottom:0;right: 0;") : "transform: ".concat(isOpen ? 'translateY(0%)' : 'translateY(100%)', ";\n      top: ").concat(fromTop, "; left: 0; bottom:0;right: 0;");
 });
 
-var StyledCloseButton = _styledComponents["default"].button(_templateObject4());
+var StyledCloseButton = _styledComponents["default"].button(_templateObject4 || (_templateObject4 = _taggedTemplateLiteral(["\n  appearance: none;\n  position: absolute;\n  right: 10px;\n  top: 10px;\n"])));
 
-var SlideOut = function SlideOut(props, ref) {
-  var closeComponent = props.closeComponent,
-      header = props.header,
+var SlideOut = function SlideOut(props) {
+  var bg = props.bg,
+      classNamePrefix = props.classNamePrefix,
+      closeComponent = props.closeComponent,
+      closeLabel = props.closeLabel,
+      hideClose = props.hideClose,
       isOpen = props.isOpen,
-      left = props.left,
-      noCloseComponent = props.noCloseComponent,
       noOverlay = props.noOverlay,
       _props$offsetTop = props.offsetTop,
       offsetTop = _props$offsetTop === void 0 ? '64px' : _props$offsetTop,
@@ -144,43 +119,43 @@ var SlideOut = function SlideOut(props, ref) {
       _props$slideFrom = props.slideFrom,
       slideFrom = _props$slideFrom === void 0 ? 'left' : _props$slideFrom,
       speed = props.speed,
-      style = props.style,
       _props$width = props.width,
-      width = _props$width === void 0 ? '350px' : _props$width;
+      width = _props$width === void 0 ? '350px' : _props$width,
+      _props$zIncrement = props.zIncrement,
+      zIncrement = _props$zIncrement === void 0 ? 0 : _props$zIncrement;
   return /*#__PURE__*/_react["default"].createElement(StyledWrapper, {
+    className: "".concat(classNamePrefix ? "".concat(classNamePrefix, "-") : '', "wrapper"),
     fromTop: offsetTop,
-    tabIndex: "-1"
+    tabIndex: "-1",
+    zIncrement: zIncrement
   }, /*#__PURE__*/_react["default"].createElement(StyledSlideOut, {
-    className: "slideout-sidebar",
+    bg: bg,
+    className: "".concat(classNamePrefix ? "".concat(classNamePrefix, "-") : '', "body"),
     fromTop: offsetTop,
     isOpen: isOpen,
-    left: left,
     slideFrom: slideFrom,
     padding: padding,
-    ref: ref,
     speed: speed,
-    style: style,
-    width: width
-  }, !noCloseComponent && closeComponent && /*#__PURE__*/_react["default"].createElement("div", {
+    width: width,
+    zIncrement: zIncrement
+  }, !hideClose && closeComponent && /*#__PURE__*/_react["default"].createElement("div", {
+    className: "".concat(classNamePrefix ? "".concat(classNamePrefix, "-") : '', "close-component"),
     onClick: onClose,
     onKeyDown: onCloseComponentKeyDown,
     onKeyUp: onCloseComponentKeyPress,
     onKeyPress: onCloseComponentKeyUp,
     role: "button",
     tabIndex: isOpen ? '0' : '-1'
-  }, closeComponent) || !noCloseComponent && /*#__PURE__*/_react["default"].createElement(StyledCloseButton, {
+  }, closeComponent) || !hideClose && /*#__PURE__*/_react["default"].createElement(StyledCloseButton, {
     id: "dismiss",
-    className: "slideout-button-close",
+    className: "".concat(classNamePrefix ? "".concat(classNamePrefix, "-") : '', "close-button"),
     onClick: onClose,
     onKeyDown: function onKeyDown(e) {
       a11yClick(e, onClose);
     },
     type: "button"
-  }, "CLOSE"), header && /*#__PURE__*/_react["default"].createElement("div", {
-    className: "sidebar-header"
-  }, header), props.children), !noOverlay && /*#__PURE__*/_react["default"].createElement(StyledOverlay, {
-    "aria-label": "close-overlay",
-    className: "overlay",
+  }, closeLabel || 'Close Sidebar'), props.children), !noOverlay && /*#__PURE__*/_react["default"].createElement(StyledOverlay, {
+    className: "".concat(classNamePrefix ? "".concat(classNamePrefix, "-") : '', "close-overlay"),
     isOpen: isOpen,
     fromTop: offsetTop,
     onClick: shouldCloseOnOverlayClick && onClose,
@@ -188,7 +163,8 @@ var SlideOut = function SlideOut(props, ref) {
     overlayColor: overlayColor,
     overlayOpacity: overlayOpacity,
     role: "button",
-    tabIndex: isOpen ? '0' : '-1'
+    tabIndex: isOpen ? '0' : '-1',
+    zIncrement: zIncrement
   }));
 };
 
